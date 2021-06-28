83d657d322c62c1e2add6f9f63cdc52fb9455c08
Jonathan Chang <ttjtftx@gmail.com>
2021-06-28 15:38:35 +0800

    Add option to keep folder

--- src/kaggleupload/cli.py
+++ src/kaggleupload/cli.py
@@ -1,7 +1,7 @@
 import dataclasses
 from argparse import ArgumentParser
 
-from kaggleupload.code import dataset_create_new, dataset_initialize
+from kaggleupload.code import create_dataset
 
 
 def add_args(parser):
@@ -18,7 +18,10 @@ def add_args(parser):
         "-d","--dir_mode", default='zip', type=str,help="dir mode"
     )
     parser.add_argument(
-        "--convert_to_csv", default=False, action="store_true", help="Whether or convert to csv."
+        "--convert_to_csv", default=False, action="store_true", help="Whether to convert to csv."
+    )
+    parser.add_argument(
+        "-k", "--keep_folder", default=False, action="store_true", help="Whether to keep the folder. (will be faster for folder with a lot of small files)."
     )
     return parser
 
@@ -26,8 +29,7 @@ def main():
     parser = ArgumentParser("kaggleupload CLI tool", usage="kaggleupload [<args>]")
     parser = add_args(parser)
     args = parser.parse_args()
-    metadata_path = dataset_initialize(folder=args.folder, dataname=args.dataname)
-    dataset_create_new(folder=args.folder, convert_to_csv=args.convert_to_csv, dir_mode=args.dir_mode)
+    create_dataset(dataname=args.dataname, folder=args.folder, keep_folder=args.keep_folder, convert_to_csv=args.convert_to_csv, dir_mode=args.dir_mode)
 
 if __name__ == "__main__":
     main()
--- src/kaggleupload/code.py
+++ src/kaggleupload/code.py
@@ -1,9 +1,13 @@
 import json
+import shutil
+import tempfile
+from pathlib import Path
 
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 api = KaggleApi()
 api.authenticate()
+
 
 def dataset_initialize(dataname, folder):
     metadata_path = api.dataset_initialize(folder=folder)
@@ -17,7 +21,40 @@ def dataset_initialize(dataname, folder):
     return metadata_path
 
 
-def dataset_create_new(
-    folder, public=False, quiet=False, convert_to_csv=False, dir_mode="zip"
+def _create_dataset(
+    dataname, folder, public=False, quiet=False, convert_to_csv=False, dir_mode="zip"
 ):
+
+    metadata_path = dataset_initialize(dataname, folder)
     return api.dataset_create_new(folder, public, quiet, convert_to_csv, dir_mode)
+
+
+def _create_dataset_keep_folder(
+    dataname, folder, public=False, quiet=False, convert_to_csv=False, dir_mode="zip"
+):
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        zipfilename = str(Path(tmpdirname) / (Path(folder).name))
+        shutil.make_archive(zipfilename, "zip", folder)
+        # need more than 1 file otherwise kaggle will ignore the root dir
+        with open(Path(tmpdirname) / "empty.txt", "w") as f:
+            f.write("")
+        folder = tmpdirname
+        return _create_dataset(
+            dataname, folder, public, quiet, convert_to_csv, dir_mode
+        )
+
+
+def create_dataset(
+    dataname,
+    folder,
+    keep_folder=False,
+    public=False,
+    quiet=False,
+    convert_to_csv=False,
+    dir_mode="zip",
+):
+    if keep_folder:
+        fn = _create_dataset_keep_folder
+    else:
+        fn = _create_dataset
+    return fn(dataname, folder, public, quiet, convert_to_csv, dir_mode)
